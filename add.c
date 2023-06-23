626a51196b07fee80deb8d786dcf337748ff5f6c
nadir <nadir.atif.94@gmail.com>
2023-06-22 20:45:45 -0700

    correction 10

--- add.c
+++ add.c
@@ -7,27 +7,27 @@
 */
 void function_to_add_element(stack_t **h, unsigned int c)
 {
-        stack_t *hd;
-        int l = 0, a;
+	stack_t *hd;
+	int l = 0, a;
 
-        hd = *h;
-        while (h)
-        {
-                hd = hd->next;
-                l++;
-        }
-        if (l < 2)
-        {
-                fprintf(stderr, "L%d: can't add, too short\n", c);
-                fclose(bus.file);
-                free(bus.content);
-                function_to_clear_stack(*h);
-                exit(EXIT_FAILURE);
-        }
-        hd = *h;
-        a = hd->n + hd->next->n;
-        hd->next->n = a;
-        *h = hd->next;
-        free(hd);
+	hd = *h;
+	while (h)
+	{
+		hd = hd->next;
+		l++;
+	}
+	if (l < 2)
+	{
+		fprintf(stderr, "L%d: can't add, too short\n", c);
+		fclose(bus.file);
+		free(bus.content);
+		function_to_clear_stack(*h);
+		exit(EXIT_FAILURE);
+	}
+	hd = *h;
+	a = hd->n + hd->next->n;
+	hd->next->n = a;
+	*h = hd->next;
+	free(hd);
 }
 
